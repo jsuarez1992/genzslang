3ec0adb4fb14c3aa706106eed8d9d4dd459d0604
Jessica <suarez.jessica1992@hotmail.com>
2023-12-03 02:11:09 +0200

    long sentence translation. Beta phase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import tkinter as tk
+from tkinter import ttk
 
 def find_meaning(word_to_find, file_path):
     with open(file_path, 'r', newline='', encoding='utf-8') as csvfile:
@@ -21,25 +22,55 @@ def find_meaning(word_to_find, file_path):
             if f" {word_to_find_lower} " in f" {word} ":
                 return f"Meaning for '{word}': {meaning}"
 
-    return f"No similar word found for '{word_to_find}'."
+    return f"{word_to_find}"
+
+def translate_sentence(sentence, file_path):
+    words_and_meanings = []
+
+    # Split the sentence into words
+    words = sentence.split()
+
+    # Find translations for each word
+    for word in words:
+        translation = find_meaning(word, file_path)
+        words_and_meanings.append((word, translation))
+
+    # Create the translated sentence
+    # Create the translated sentence
+    translated_sentence = []
+
+    # Iterate over each phrase and its translation
+    for original_phrase, translation in words_and_meanings:
+        if translation is not None:
+            meaning_parts = translation.split(': ')
+            if len(meaning_parts) > 1:
+                translated_sentence.append(f"{meaning_parts[1]}")
+            else:
+                translated_sentence.append(f"{translation}")
+        else:
+            translated_sentence.append(original_phrase)
+
+    return " ".join(translated_sentence)
+
+
 
 def on_submit():
     user_input = entry.get()
-    result = find_meaning(user_input, "genz_slang.csv")  # Replace with your actual file path
+    result = translate_sentence(user_input, "genz_slang.csv")  # Replace with your actual file path
     result_label.config(text=result)
 
 # Create the main window
 root = tk.Tk()
-root.title("Jessica's Gen-Z Translator")
+root.title("Word Translator")
 
 # Add margin to the sides
-root.geometry("400x200+100+100")  # width x height + x_offset + y_offset
+root.geometry("600x200+100+100")  # width x height + x_offset + y_offset
 
 # Create and place widgets with added margins
-label = tk.Label(root, text="Enter a word:")
+label = tk.Label(root, text="Enter a sentence:")
 label.pack(pady=10, padx=20)
 
-entry = tk.Entry(root)
+entry = tk.Entry(root, width=50)
 entry.pack(pady=10, padx=20)
 
 submit_button = tk.Button(root, text="Submit", command=on_submit)
@@ -50,4 +81,3 @@ result_label.pack(pady=10, padx=20)
 
 # Start the main event loop
 root.mainloop()
-
